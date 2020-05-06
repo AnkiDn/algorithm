96c8ff478b5a98b0549ecce1a5eb3eb463b68a8a
wangxuan <ankidnn@gmail.com>
2020-05-06 17:26:28 +0800

    sqrt x solution

---
+++ 69.sqrt-x.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=69 lang=java
+ *
+ * [69] Sqrt(x)
+ *
+ * https://leetcode.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (33.19%)
+ * Likes:    1171
+ * Dislikes: 1781
+ * Total Accepted:    517.6K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '4'
+ *
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x, where x is guaranteed to be a
+ * non-negative integer.
+ * 
+ * Since the return type is an integer, the decimal digits are truncated and
+ * only the integer part of the result is returned.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 4
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since 
+ * the decimal part is truncated, 2 is returned.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt1(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        // 注意：针对特殊测试用例，例如 2147395599
+        // 要把搜索的范围设置成长整型
+        long left = 1;
+        long right = x / 2;
+        while (left < right) {
+            // 注意：这里一定取右中位数，如果取左中位数，代码会进入死循环
+            // long mid = left + (right - left + 1) / 2;
+            long mid = (left + right + 1) >>> 1;
+            long square = mid * mid;
+            if (square > x) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        // 因为一定存在，因此无需后处理
+        return (int) left;
+    }
+
+    /**
+     * 牛顿迭代法
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) {
+        double a = x;
+        double pre = 0.0;
+        do {
+            pre = a;
+            a = ( a + x / a ) / 2;
+        } while (Math.abs(pre - a) >= 1);
+        return (int) a;
+    }
+}
+// @lc code=end
+
