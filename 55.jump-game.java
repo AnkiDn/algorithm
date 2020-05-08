35df26e092fbef453c416e02b4aec7960fe28b33
wangxuan <ankidnn@gmail.com>
2020-05-08 16:20:54 +0800

    jump game solution

---
+++ 55.jump-game.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=55 lang=java
+ *
+ * [55] Jump Game
+ *
+ * https://leetcode.com/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (33.44%)
+ * Likes:    3746
+ * Dislikes: 314
+ * Total Accepted:    440.3K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * Given an array of non-negative integers, you are initially positioned at the
+ * first index of the array.
+ * 
+ * Each element in the array represents your maximum jump length at that
+ * position.
+ * 
+ * Determine if you are able to reach the last index.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last
+ * index.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum
+ * jump length is 0, which makes it impossible to reach the last index.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 3 * 10^4
+ * 0 <= nums[i][j] <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 贪心
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > max) return false; //max 至少要 >= i才能一直跳，否则肯定到不了
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
+    }
+}
+// @lc code=end
+
