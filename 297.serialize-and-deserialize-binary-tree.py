be73bc6a0b3a61b3c28bb3490df8a40deaca3012
wangxuan <ankidnn@gmail.com>
2020-05-15 17:00:29 +0800

    seriallize and deserialize binary tree py solution

---
+++ 297.serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=297 lang=python3
+#
+# [297] Serialize and Deserialize Binary Tree
+#
+# https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+#
+# algorithms
+# Hard (45.80%)
+# Likes:    2648
+# Dislikes: 131
+# Total Accepted:    293K
+# Total Submissions: 637.9K
+# Testcase Example:  '[1,2,3,null,null,4,5]'
+#
+# Serialization is the process of converting a data structure or object into a
+# sequence of bits so that it can be stored in a file or memory buffer, or
+# transmitted across a network connection link to be reconstructed later in the
+# same or another computer environment.
+# 
+# Design an algorithm to serialize and deserialize a binary tree. There is no
+# restriction on how your serialization/deserialization algorithm should work.
+# You just need to ensure that a binary tree can be serialized to a string and
+# this string can be deserialized to the original tree structure.
+# 
+# Example: 
+# 
+# 
+# You may serialize the following tree:
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   3
+# ⁠    / \
+# ⁠   4   5
+# 
+# as "[1,2,3,null,null,4,5]"
+# 
+# 
+# Clarification: The above format is the same as how LeetCode serializes a
+# binary tree. You do not necessarily need to follow this format, so please be
+# creative and come up with different approaches yourself.
+# 
+# Note: Do not use class member/global/static variables to store states. Your
+# serialize and deserialize algorithms should be stateless.
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        def rserialize(root, string):
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.val) + ','
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
+        
+        return rserialize(root, '')
+
+    def deserialize(self, data):
+        def rdeserialize(l):
+            val = l.pop(0)
+            if val == 'None':
+                return None
+                
+            root = TreeNode(val)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+# @lc code=end
+
