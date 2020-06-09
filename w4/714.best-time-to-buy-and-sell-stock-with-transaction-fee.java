3278290eb59fffa9574704c27173aa8e5d735e39
wangxuan <ankidnn@gmail.com>
2020-06-09 17:54:17 +0800

    best time to buy and sell stock with transaction fee solution

---
+++ w4/714.best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=714 lang=java
+ *
+ * [714] Best Time to Buy and Sell Stock with Transaction Fee
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+ *
+ * algorithms
+ * Medium (53.80%)
+ * Likes:    1441
+ * Dislikes: 41
+ * Total Accepted:    65K
+ * Total Submissions: 120.7K
+ * Testcase Example:  '[1,3,2,8,4,9]\n2'
+ *
+ * Your are given an array of integers prices, for which the i-th element is
+ * the price of a given stock on day i; and a non-negative integer fee
+ * representing a transaction fee.
+ * You may complete as many transactions as you like, but you need to pay the
+ * transaction fee for each transaction.  You may not buy more than 1 share of
+ * a stock at a time (ie. you must sell the stock share before you buy again.)
+ * Return the maximum profit you can make.
+ * 
+ * Example 1:
+ * 
+ * Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+ * Output: 8
+ * Explanation: The maximum profit can be achieved by:
+ * Buying at prices[0] = 1Selling at prices[3] = 8Buying at prices[4] =
+ * 4Selling at prices[5] = 9The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) =
+ * 8.
+ * 
+ * 
+ * 
+ * Note:
+ * 0 < prices.length .
+ * 0 < prices[i] < 50000.
+ * 0 .
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * dp
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][1] = -(prices[0] + fee);
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+        return dp[n - 1][0];
+    }
+
+    /**
+     * 状态压缩 dp
+     */
+    public int maxProfit2(int[] prices, int fee) {
+        int n = prices.length;
+        int[] dp = new int[2];
+        dp[1] = -(prices[0] + fee);
+        for (int i = 1; i < n; ++i) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], dp[0] - prices[i] - fee);
+        }
+        return dp[0];
+    }
+}
+// @lc code=end
+
