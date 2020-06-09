0765812cbb095547bb63e3d27d3c4169e9739523
wangxuan <ankidnn@gmail.com>
2020-06-09 16:31:49 +0800

    best time to buy and sell stock ii solution

---
+++ w4/122.best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=122 lang=java
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ *
+ * algorithms
+ * Easy (56.14%)
+ * Likes:    2278
+ * Dislikes: 1735
+ * Total Accepted:    590.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array prices for which the i^th element is the price of a
+ * given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (i.e., buy one and sell one share of the stock
+ * multiple times).
+ * 
+ * Note: You may not engage in multiple transactions at the same time (i.e.,
+ * you must sell the stock before you buy again).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit
+ * = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+ * 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
+ * = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you
+ * are
+ * engaging multiple transactions at the same time. You must sell before buying
+ * again.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 3 * 10 ^ 4
+ * 0 <= prices[i] <= 10 ^ 4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            if (i == 0) {
+                dp[0][0] = 0;
+                dp[0][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public int maxProfit2(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int[][] dp = new int[n + 1][2];
+        dp[0][1] = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
+        }
+        return dp[n][0];
+    }
+
+    public int maxProfit3(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int[] dp = new int[2];
+        dp[1] = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; ++i) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i - 1]);
+            dp[1] = Math.max(dp[1], dp[0] - prices[i - 1]);
+        }
+        return dp[0];
+    }
+}
+// @lc code=end
+
